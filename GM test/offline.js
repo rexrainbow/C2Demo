b0a01288700bf547b90ba9e0bec283dba4a25475
Hua <binhua.tein@gmail.com>
2016-11-12 21:58:17 +0800

    Update

---
+++ GM test/offline.js
@@ -0,0 +1,15 @@
+﻿{
+	"version": 1478959040,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"html2canvas.min.js"
+	]
+}
